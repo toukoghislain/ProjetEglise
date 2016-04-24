f3b37606b6e33c94d122e09a410b1061713f5ec6
cloclo <clo@gmail.com>
2016-04-25 01:22:56 +0200

    second commit

--- ApplicationEglise/Controllers/HomeController.cs
+++ ApplicationEglise/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace ApplicationEglise.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page. cl";
 
             return View();
         }
